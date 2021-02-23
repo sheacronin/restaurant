4bc4270fc57ad3033f3db30be3af340b03a2e811
Shea Cronin <shealcronin@gmail.com>
2021-02-23 17:08:48 -0500

    Add initial functionality for displaying menu items using objects

--- src/menu.js
+++ src/menu.js
@@ -5,18 +5,27 @@ class MenuItem {
     constructor(name, price, img) {
         this.name = name;
         this.price = price;
-        this.img = img;
+        this.img = '../src/menu-items/' + img;
     }
+
+
 }
+
+const peach = new MenuItem('peach cobbler', 7, 'peach.jpg');
+
+console.log(MenuItem);
 
 function showMenu() {
     const menu = appendNewElement('div', content, 'menu');
 
-    const menuItems = ['spaghetti', 'mushroom risotto', 'peach cobbler',
+    const menuItems = [peach, 'spaghetti', 'mushroom risotto', 'peach cobbler',
     'turtle soup', 'calamari'];
 
     for (let i = 0; i < menuItems.length; i++) {
-        appendNewElement('div', menu, 'menu-item', menuItems[i]);
+        const item = appendNewElement('div', menu, 'menu-item', menuItems[i].name);
+        const img = document.createElement('img');
+        img.src = menuItems[i].img;
+        item.appendChild(img);
     }
 }
 
